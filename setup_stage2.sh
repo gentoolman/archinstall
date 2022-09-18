136237688b906bd01ad204422b462370c5d4cad8
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 22:23:13 +0200

    Update setup_stage2.sh

--- setup_stage2.sh
+++ setup_stage2.sh
@@ -1,11 +1,13 @@
-#!/usr/bin
+#!/usr/bin/env bash
 
 # Sys Clock
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
+
 # Locale
 sed --in-place=.bak 's/^#de_DE\.UTF-8/de_DE\.UTF-8/' /etc/locale.gen
 locale-gen
 echo LANG=de_DE.UTF-8 > /etc/locale.conf
+
 # Set Hostname 
 echo $hostname > /etc/hostname
