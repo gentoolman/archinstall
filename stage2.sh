493339916533358be46fd25992bc86f5b087408e
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 22:45:08 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -17,7 +17,6 @@ echo LANG=de_DE.UTF-8 > /etc/locale.conf
 echo "KEYMAP=de-latin1-nodeadkeys" > /etc/vconsole.conf
 
 # Hosts file
-echo "" >> /etc/hosts
 echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts
 echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
