de6fd1f4aa337ba130ef1e77beb4d4af90809c53
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 20:15:55 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,8 +9,10 @@ read -srep "Enter Disk Encryption Password: " cryptvar
 read -sp "Confirm Disk Encryption Password: " cryptvarconfirm
 if [ "$cryptvar" == "$cryptvarconfirm" ]; then
     echo "Starting installation.."
-    break
 else
     echo "Password do not Match! Aborting installation.."
     exit
 fi
+
+# Partitioning
+echo "Starting Partitioning.."
