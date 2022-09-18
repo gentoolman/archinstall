f319bb6894b150f3f13d878adcdf02f6bf4bc65d
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-19 00:12:04 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 blkid -s UUID -o value /dev/sda3 > uuid.tmp
 uuid=$(<uuid.tmp)
-sed --in-place=.bak 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevicetest"/' /etc/default/grub
+sed --in-place=.bak 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID=$uuid:cryptdisk root=\/dev\/mapper\/cryptdisk"/' /etc/default/grub
 rm -f uuid.tmp
