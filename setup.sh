a1bca17d5ca334923a9a78b1597bf25ef5709d00
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 20:17:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "Using $drivevar.."
 # Disk Encryption Selection
 read -srep "Enter Disk Encryption Password: " cryptvar
 read -sp "Confirm Disk Encryption Password: " cryptvarconfirm
+echo "Correct :3"
 if [ "$cryptvar" == "$cryptvarconfirm" ]; then
     echo "\nStarting installation.."
 else
