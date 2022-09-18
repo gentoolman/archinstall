c99dc4fe938fb0b9aee1fab1c7c4da9cfaadcba3
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 23:48:11 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -40,4 +40,5 @@ grub-mkconfig -o /boot/grub/grub.cfg
 blkid -s UUID -o value /dev/sda3 > uuid.tmp
 uuid="cat uuid.tmp"
 sed --in-place=.bak 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='$uuid':cryptdisk root=/dev/mapper/cryptdisk"/' /etc/default/grub
+rm -f uuid.tmp
 grub-mkconfig -o /boot/grub/grub.cfg
