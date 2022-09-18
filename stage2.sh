999d34374fbafc83915fefce03769da60af74bfd
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 22:26:18 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set Hostname 
+read -p "Enter Hostname: " sysdrive
+echo $hostname > /etc/hostname
+
 # Sys Clock
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
@@ -8,6 +12,3 @@ hwclock --systohc
 sed --in-place=.bak 's/^#de_DE\.UTF-8/de_DE\.UTF-8/' /etc/locale.gen
 locale-gen
 echo LANG=de_DE.UTF-8 > /etc/locale.conf
-
-# Set Hostname 
-echo $hostname > /etc/hostname
