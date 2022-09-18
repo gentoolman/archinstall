ba3e46572033b67c5a7c515ed9b6e6ec8eafedf8
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-19 00:14:39 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Grub Installation
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-blkid -s UUID -o value /dev/sda3 > uuid.tmp
-uuid=$(<uuid.tmp)
-sed --in-place=.bak 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='$uuid':cryptdisk root=\/dev\/mapper\/cryptdisk"/' /etc/default/grub
-rm -f uuid.tmp
