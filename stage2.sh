b4fad4c28fde1d23f97eca63116afc67964af58f
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 23:26:32 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -37,4 +37,6 @@ mkinitcpio -p linux
 # Grub Installation
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
-blkid -s UUID -o value /dev/sda3
+uuid=$(blkid -s UUID -o value /dev/sda3)
+sed --in-place=.bak 's/^GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=UUID='$uuid':cryptdisk root=/dev/mapper/cryptdisk"/' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
