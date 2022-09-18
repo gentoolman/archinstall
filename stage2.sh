2a4a7f00d66203a2101c7b03735e5d1ba286d219
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-18 22:41:53 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -12,3 +12,12 @@ hwclock --systohc
 sed --in-place=.bak 's/^#de_DE\.UTF-8/de_DE\.UTF-8/' /etc/locale.gen
 locale-gen
 echo LANG=de_DE.UTF-8 > /etc/locale.conf
+
+# Vconsole
+echo "KEYMAP=de-latin1-nodeadkeys" > /etc/vconsole.conf
+
+# Hosts file
+echo "" >> /etc/hosts
+echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
+echo -e "::1\t\tlocalhost" >> /etc/hosts
+echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
