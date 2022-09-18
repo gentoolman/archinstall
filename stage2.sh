78e23e3c3df24f7dc420bebc3adf3349ff42d538
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-19 01:14:35 +0200

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -38,7 +38,7 @@ mkinitcpio -p linux
 mount /dev/sda1 /boot
 
 # Grub Installation
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 blkid -s UUID -o value /dev/sda3 > uuid.tmp
 uuid=$(<uuid.tmp)
