e9400c664bf41080d2e2dcdc6e2488c976a88203
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-20 00:39:10 +0200

    Update stage1.sh

--- stage1.sh
+++ stage1.sh
@@ -3,17 +3,6 @@
 # Drive Selection
 read -p "[Select Drive]: " sysdrive
 
-# Disk Encryption Selection
-read -srep "[Enter Disk Encryption Password]: " pass
-read -sp "[Confirm Disk Encryption Password]: " passconfirm
-echo "*"
-if [ "$pass" == "$passconfirm" ]; then
-    echo "Starting Installation on $sysdrive.."
-else
-    echo "Password do not Match! Aborting installation.."
-    exit
-fi
-
 # Partitioning
 wipefs $sysdrivehttps://github.com/archungus333/archinstall/blob/main/stage1.sh
 (
@@ -39,8 +28,6 @@ wipefs $sysdrivehttps://github.com/archungus333/archinstall/blob/main/stage1.sh
 mkfs.vfat -F32 -n EFI $sysdrive"1"
 
 # Setup Encrypted LUKS
-# echo $pass | cryptsetup --use-random luksFormat $sysdrive"2" -d -
-# echo $pass | cryptsetup luksOpen $sysdrive"2" luks -d -
 cryptsetup --use-random luksFormat $sysdrive"2"
 cryptsetup luksOpen $sysdrive"2" luks
 
