03dc9fec42bef4f11de2a4220db154b1113e6e35
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-20 13:06:24 +0200

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -5,23 +5,24 @@ sudo pacman -Syyu
 sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty nano neofetch cmatrix htop python-pip python-pywal lightdm lightdm-gtk-greeter dmenu
 
 # YAY
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd ..
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
 rm -rf yay
 
 # .Config Struktur
-mkdir –p $HOME/.config/bspwm/
-mkdir $HOME/.config/sxhkd/
-mkdir $HOME/.config/polybar/
+mkdir –p ~/.config/bspwm/
+mkdir ~/.config/sxhkd/
+mkdir ~/.config/polybar/
 
-cp /etc/polybar/config.ini $HOME/.config/polybar/config.ini
-cd .config/polybar
+# Polybar Init Config
+cp /etc/polybar/config.ini ~/.config/polybar/config.ini
+cd ~/.config/polybar
 echo "#!/bin/bash
 killall -q polybar
 polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Polybar launched..."" > launch.sh
-cd
+echo "Polybar launched..."" > launch.sh && cd
 
 install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
 install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
+# Systemd Symlinks
 systemctl enable lightdm.service
