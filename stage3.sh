012ae91b562410f42fe0ccce71d971f9302291a2
Gentoolman <batu.zeren@gmail.com>
2022-09-20 10:08:09 +0200

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -9,10 +9,11 @@ git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd && rm -rf yay/
 
 #copying default configs
-mkdir $HOME/.config/bspwm
-mkdir $HOME/.config/sxhkd
-mkdir $HOME/.config/polybar 
-cp etc/polybar/config.ini $HOME/.config/polybar/config.ini
+mkdir $HOME/.config/bspwm/
+mkdir $HOME/.config/sxhkd/
+mkdir $HOME/.config/polybar/
+
+cp /etc/polybar/config.ini $HOME/.config/polybar/config.ini
 cd .config/polybar && touch launch.sh
 echo "#!/bin/bash
 
@@ -27,6 +28,7 @@ polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 echo "Polybar launched..."" > launch.sh
 chmod +x launch.sh
 cd
+
 install -Dm755 /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/bspwmrc
 install -Dm644 /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/sxhkdrc
 
