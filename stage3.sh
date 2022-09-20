2c33d960868b496b10d6601d5f8cb5dff42a349b
archungus <113145908+archungus333@users.noreply.github.com>
2022-09-20 13:29:04 +0200

    Update stage3.sh

--- stage3.sh
+++ stage3.sh
@@ -11,7 +11,7 @@ sudo pacman -Syyu
 # Desktop Env
 sudo pacman -S --noconfirm --needed bspwm sxhkd polybar picom nitrogen kitty lightdm lightdm-gtk-greeter dmenu
 # Misc
-sudo pacman -S --noconfirm --needed vim nvim nano htop neofetch cmatrix python python-pip python-pywal
+sudo pacman -S --noconfirm --needed nano neovim vim htop neofetch cmatrix python python-pip python-pywal
 
 # YAY
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd
