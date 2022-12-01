d48ab243cad7e3cc40c03de64ec3c02bcb8cb8ac
whatev3r <113145908+archungus333@users.noreply.github.com>
2022-12-01 11:14:38 +0100

    Update stage2.sh

--- stage2.sh
+++ stage2.sh
@@ -36,7 +36,7 @@ echo -e "127.0.1.1\t$hostname.localdomain\t$hostname" >> /etc/hosts
 
 # Pacman
 pacman -Syyu
-pacman --noconfirm -S networkmanager iwd wireless_tools mtools reflector linux-headers xdg-utils xdg-user-dirs 
+pacman --noconfirm -S networkmanager iwd wireless_tools mtools reflector linux-headers xdg-utils xdg-user-dirs dhclient dhcpd
 
 # Mkinitcpio Config
 sed --in-place=.bak 's/^MODULES=()/MODULES=(ext4)/' /etc/mkinitcpio.conf
